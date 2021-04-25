deba40fa01bec473a7184ff09880b3fa04f7fc30
ashleyg101 <ashleygong2000@yahoo.com>
2021-04-25 13:18:19 -0400

    added parse, googleMaps, googlePlaces pods; created screens, navigation flow, tab bar controller, navigation controllers, view controllers; login, categories

---
+++ ParkSleuth/CategoriesViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CategoriesViewController.swift
+//  ParkSleuth
+//
+//  Created by Ashley Gong on 4/24/21.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    
+    @IBAction func skateParkBtn(_ sender: Any) {
+    }
+    @IBAction func basketballBtn(_ sender: Any) {
+    }
+    @IBAction func tennisCourtBtn(_ sender: Any) {
+    }
+    @IBAction func rollerRinkBtn(_ sender: Any) {
+    }
+    @IBAction func soccerFieldBtn(_ sender: Any) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
