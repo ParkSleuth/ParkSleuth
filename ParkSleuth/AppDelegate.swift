3e8358161aa68bb92f762e69d6d768eec0a2acca
perlaescano <perlaescanoe@gmail.com>
2021-04-29 22:09:23 -0400

    Parse configuration: Login and Sign up (Tested)

--- ParkSleuth/AppDelegate.swift
+++ ParkSleuth/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                  $0.applicationId = "rNg4Z9ehAQzkae5FPVrDRv8f0wlri8b179CMcY4S"
+                  $0.clientKey = "vzY7bbULE5Mgsv0s5QZc6ReL8l4tUxqKAzfkox9M"
+                  $0.server = "https://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
