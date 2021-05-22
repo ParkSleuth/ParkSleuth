05284add3b4c453e553ef7747b64c9333c5f3c94
ashleyg101 <ashleygong2000@yahoo.com>
2021-05-22 13:02:21 -0400

    text search request for skateparks in nyc http request

--- ParkSleuth/MapViewController.swift
+++ ParkSleuth/MapViewController.swift
@@ -12,7 +12,7 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let url = URL(string:"https://maps.googleapis.com/maps/api/place/textsearch/json?query=skateparks+in+nyc&key=AIzaSyDvFkv2h6M4O1GSkR1xz_b2v5UU21Ps8zo")!
+        let url = URL(string:"https://maps.googleapis.com/maps/api/place/textsearch/json?query=skateparks+in+nyc&key=[key]")!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
