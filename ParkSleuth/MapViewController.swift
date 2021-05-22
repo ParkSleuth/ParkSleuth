279dfe220e1aa766669c186f0af15f34c043de03
ashleyg101 <ashleygong2000@yahoo.com>
2021-05-21 22:33:52 -0400

    query skateparks using googleplaces API nearby search request

--- ParkSleuth/MapViewController.swift
+++ ParkSleuth/MapViewController.swift
@@ -12,10 +12,26 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let url = URL(string:"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&keyword=skatepark&key=AIzaSyDvFkv2h6M4O1GSkR1xz_b2v5UU21Ps8zo")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+         if let error = error {
+                print(error.localizedDescription)
+         } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                print(dataDictionary)
+            
+                // Get array of parks
+                // Store parks in a property
+                // Reload mapview and display parks
+         }
+        }
+        task.resume()
     }
     
-
     /*
     // MARK: - Navigation
 
